2bd3585986da733ea4d18b2f8f95c436e7617421
Estuardo-Sazo <estuardosazo1206@gmail.com>
2021-05-06 16:36:04 -0600

    Modificacion de expenses

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const config = require('../config/index');
 const groups = require('./components/groups/network');
 const subGroups = require('./components/sub-groups/network');
+const expenses = require('./components/expenses/network');
+
 const errors = require('../network/errors');
 
 const app = express();
@@ -15,11 +17,13 @@ app.use(bodyParser.json());
 // ROUTER
 app.use('/api/groups', groups);
 app.use('/api/sub-groups', subGroups);
+app.use('/api/expenses', expenses);
+
 
 
 
 app.use(errors);
 
 app.listen(config.api.port, () => {
-    console.log('API escuchando en el puerto ', config.api.port);
+    console.log('API escuchando en el puerto ','http://localhost:'+ config.api.port+'/api/');
 })
